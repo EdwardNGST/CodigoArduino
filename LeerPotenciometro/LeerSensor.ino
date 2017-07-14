3b07663ca958f5f3a21d1809828971330bd050ba
EdwardNGST <alan_hernandezgarcia@hotmail.com>
2017-07-13 22:47:45 -0500

    Add files via upload

---
+++ LeerPotenciometro/LeerSensor.ino
@@ -0,0 +1,26 @@
+/*
+* Practica 07: Lectura analogica de un sensor (potenciometro).
+*              Mostrar a lectura en el monitor serial.
+*
+*
+*/
+  // Variables globales
+  int pot = 0;
+  int lectura;
+
+void setup() {
+  // put your setup code here, to run once:
+  //Inicializacion del puerto serial
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Lectura del potenciometro
+  lectura = analogRead(pot);
+
+  // Escribir al puerto serial
+  Serial.print("Lectura potenciometro: ");
+  Serial.println(lectura);
+
+}
